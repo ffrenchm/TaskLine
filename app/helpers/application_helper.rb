d5feb3dbd4c59b2cc8a6a30118cb3d3878b6f5bb
Matt Ffrench <ffrench.m@hotmail.com>
2017-07-30 22:09:54 +0100

    Add font awesome, todo_list and todo_item scaffolds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Trial App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def resource_name
+    :user
+  end
+
+  def resource_class
+     User
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
 end
