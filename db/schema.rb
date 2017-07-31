e6513f2a6b145aed4ad21bc8b17d459ca51f0521
Matt Ffrench <ffrench.m@hotmail.com>
2017-07-31 23:00:18 +0100

    Remove devise and list scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730192016) do
+ActiveRecord::Schema.define(version: 20170731205945) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170730192016) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_todo_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
