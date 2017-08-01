bd22efae5e862c0071ff3251c1abcaa2b8da4cfe
Matt Ffrench <ffrench.m@hotmail.com>
2017-08-01 09:09:25 +0100

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  post 'signin', to 'sessions#create'
+  post 'signin', to: 'sessions#create'
 
   resources :users
 end
