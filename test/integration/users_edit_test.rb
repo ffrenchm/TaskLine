15f4f0ac830814e18df15b6444cf329668bde171
Matt Ffrench <ffrench.m@hotmail.com>
2017-08-07 08:58:54 +0100

    Complete update user form and its tests

---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:matt)
+  end
+
+  test "unsuccessful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { email: 'invalid@email',
+                                              password: 'foo',
+                                              password_confirmation: 'bar' } }
+    assert_template 'users/edit'
+  end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user: { email: "valid@email.com",
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.email, "valid@email.com"
+  end
+end
