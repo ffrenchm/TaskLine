98c38353ff014dd48d2813259789521e301db27d
Matt Ffrench <ffrench.m@hotmail.com>
2017-08-13 09:35:56 +0100

    Insert nested routes and edit user show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :category
+  belongs_to :user
+  default_scope -> { order(:deadline) }
   validates :category_id, presence: true
+  validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 155}
 end
