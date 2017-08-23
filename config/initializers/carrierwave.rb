77137a825464cb6858efe7a65ba97e484d2243e8
Matt Ffrench <ffrench.m@hotmail.com>
2017-08-24 00:11:14 +0100

    Add carrierwave iniitializer file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,23 +4,14 @@ if Rails.env.test?
   end
 end
 
-if Rails.env.test? or Rails.env.development?
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.root = "#{Rails.root}/tmp"
-    config.cache_dir = "#{Rails.root}/tmp/images"
-  end
-else
+if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      :provider => 'AWS',
-      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => ENV['AWS_S3_REGION']
+      # Configuration for Amazon S3
+      :provider              => 'AWS',
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory = ENV['AWS_S3_BUCKET_NAME']
-    config.asset_host = "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['AWS_S3_BUCKET_NAME']}"
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
